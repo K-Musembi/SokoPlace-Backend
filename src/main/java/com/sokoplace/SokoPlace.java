b150b0ffc769f0c32171844fa53db36e0b100a5b
K-Musembi <kevinmusembi.m@gmail.com>
2025-04-07 17:15:30 +0300

    initial commit; project setup

---
+++ src/main/java/com/sokoplace/SokoPlace.java
@@ -0,0 +1,13 @@
+package com.sokoplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SokoPlace {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SokoPlace.class, args);
+	}
+
+}
