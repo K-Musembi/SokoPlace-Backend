34f5f2b9a2a1efcb7f5f1a56afc34d7dbbe54428
K-Musembi <kevinmusembi.m@gmail.com>
2025-04-22 18:03:02 +0300

    service layer for Product model / entity

--- src/main/java/com/sokoplace/product/ProductRepository.java
+++ src/main/java/com/sokoplace/product/ProductRepository.java
@@ -3,11 +3,16 @@ package com.sokoplace.product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    Product findByCategory(String category);
-    Product findByBrand(String Category, String brand);
+    //Spring Data JPA reads the method names in your repository interface and automatically
+    // generates the corresponding database query based on those names and parameters.
+
+    List<Product> findByCategory(String category);
+    List<Product> findByBrand(String Category, String brand);
     Product findByModel(String Category, String brand, String model);
 }
 
---
+++ src/main/java/com/sokoplace/product/ProductService.java
@@ -0,0 +1,111 @@
+package com.sokoplace.product;
+
+//  Though you could inject repositories directly into controllers,
+//  it is best practice to have a service layer.
+
+import com.sokoplace.product.dto.ProductRequest;
+import com.sokoplace.product.dto.ProductResponse;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Transactional
+    public ProductResponse createProduct(ProductRequest productRequest) {
+        if (productRepository.findByModel(
+                productRequest.category(),
+                productRequest.brand(),
+                productRequest.model()) != null) {
+            throw new IllegalArgumentException("Product already exists");
+        }
+
+        Product product = new Product();
+        Product createdProduct = getProduct(product, productRequest);
+        productRepository.save(createdProduct);
+        return mapToProductResponse(createdProduct);
+    }
+
+    @Transactional
+    public ProductResponse findProductById(Long Id) {
+        Product product = productRepository.findById(Id)
+                .orElseThrow(() -> new RuntimeException("Product not found"));
+        return mapToProductResponse(product);
+    }
+
+    @Transactional
+    public List<ProductResponse> findProductByCategory(String category) {
+        List<Product> products = productRepository.findByCategory(category);
+
+        return products.stream()
+                .map(this::mapToProductResponse)
+                .toList();
+    }
+
+    @Transactional
+    public List<ProductResponse> findProductByBrand(String category, String brand) {
+        List<Product> products = productRepository.findByBrand(category, brand);
+        return products.stream()
+                .map(this::mapToProductResponse)
+                .toList();
+    }
+
+    @Transactional
+    public ProductResponse findProductByModel(String category, String brand, String model) {
+        Product product = productRepository.findByModel(category, brand, model);
+        return mapToProductResponse(product);
+    }
+
+    @Transactional
+    public ProductResponse updateProduct(Long Id, ProductRequest productRequest) {
+        Product product = productRepository.findById(Id)
+                .orElseThrow(() -> new RuntimeException("Product not found"));
+
+        Product updatedProduct = getProduct(product, productRequest);
+        productRepository.save(updatedProduct);
+        return mapToProductResponse(updatedProduct);
+    }
+
+    @Transactional
+    public ProductResponse deleteProduct(Long Id) {
+        Product product = productRepository.findById(Id)
+                .orElseThrow(() -> new RuntimeException("Product not found"));
+        productRepository.delete(product);
+        return mapToProductResponse(product);
+    }
+
+    // Similar lines repeated for createProduct and updateProduct. Common method created
+    private Product getProduct(Product product, ProductRequest productRequest) {
+        product.setCategory(productRequest.category());
+        product.setBrand(productRequest.brand());
+        product.setModel(productRequest.model());  // setter method, generated through @Data (Lombok) in model file
+        product.setPrice(productRequest.price());
+        product.setDescription(productRequest.description());
+        product.setImageUrl("resources/images/" + productRequest.category() + "/"
+                + productRequest.brand() + "/" + productRequest.model() + ".jpg");
+        return product;
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return new ProductResponse(
+                product.getId(),
+                product.getCategory(),  // getter method
+                product.getBrand(),
+                product.getModel(),
+                product.getPrice(),
+                product.getDescription(),
+                product.getImageUrl()
+        );
+    }
+
+}
