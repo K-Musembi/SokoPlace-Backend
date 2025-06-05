adb928dd0d24d8644dd7938f5118814040a46ce3
K-Musembi <kevinmusembi.m@gmail.com>
2025-06-05 15:46:24 +0300

    configured CORS security

---
+++ src/main/java/com/sokoplace/config/security/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.sokoplace.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req -> req
+                                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()  // allow CORS preflight requests
+                                .requestMatchers("/api/v1/**").permitAll()
+                                .anyRequest()
+                                .authenticated()
+                );
+        return http.build();
+    }
+}
